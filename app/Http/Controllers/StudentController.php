72021f3575848829f74c509c0c0f6d1ae3d4519f
waqas butt <wb@onyxtec.com>
2022-01-27 11:48:40 +0500

    first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function show(student $student)
+    {
+
+        // $students = student::all();
+        $students= student::where('name','waqas')->get();
+        return view('welcome',['students'=> $students]);
+    }
+}
